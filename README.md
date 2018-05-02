07f08cf17b0e608315b7a5690054d0de2c7c7f0d
WulfMarius <WulfMarius@users.noreply.github.com>
2018-05-02 18:34:01 +0200

    sort fire starters first by type, then by chance, then by name

---
+++ README.md
@@ -0,0 +1,16 @@
+Sort-Fire-Starters
+
+
+This is a mod for **The Long Dark** by Hinterland Studio, Inc.
+
+
+It changes the sort order of fire starters when building a fire.  
+The original sort order is strictly by name, which would sometimes put unusable items before usable ones (e.g. "Magnifying Lens" before "Wood Matches" when there is no sunlight)
+
+This mod will put burning items (Torch, Flare) first, then "normal" fire starters (Matches, Fire Striker), and then those needing sunlight (Magnifying Lens, [Binoculars](https://github.com/WulfMarius/Binoculars)).  
+Each category will be sorted by chance to start the fire (highest chance first) and then by name.
+
+
+Requires the [Mod Loader](https://github.com/zeobviouslyfakeacc/ModLoaderInstaller) to run.
+
+Can be installed with the [Mod-Installer](https://github.com/WulfMarius/Mod-Installer).
---
+++ src/Panel_StartFire_Patches.cs
@@ -0,0 +1,68 @@
+﻿using Harmony;
+using System.Collections.Generic;
+
+namespace SortFireStarters
+{
+    [HarmonyPatch(typeof(Panel_FireStart), "RefreshList")]
+    internal class Panel_StartFire_RefreshList
+    {
+        internal static void Postfix(List<GearItem> gearList, FireStartMaterialType type)
+        {
+            if (type == FireStartMaterialType.FireStarter)
+            {
+                try
+                {
+                    gearList.Sort(CompareFireStarters);
+                }
+                catch (System.Exception e)
+                {
+                    UnityEngine.Debug.LogException(e);
+                }
+            }
+        }
+
+        private static int CompareFireStarters(GearItem item1, GearItem item2)
+        {
+            if (item1.m_TorchItem?.IsBurning() == true && item2.m_TorchItem?.IsBurning() != true)
+            {
+                return -1;
+            }
+
+            if (item1.m_TorchItem?.IsBurning() != true && item2.m_TorchItem?.IsBurning() == true)
+            {
+                return 1;
+            }
+
+            if (item1.m_FlareItem?.IsBurning() == true && item2.m_FlareItem?.IsBurning() != true)
+            {
+                return -1;
+            }
+
+            if (item1.m_FlareItem?.IsBurning() != true && item2.m_FlareItem?.IsBurning() == true)
+            {
+                return 1;
+            }
+
+            FireStarterItem fireStarter1 = item1.m_FireStarterItem;
+            FireStarterItem fireStarter2 = item2.m_FireStarterItem;
+
+            if (!fireStarter1.m_RequiresSunLight && fireStarter2.m_RequiresSunLight)
+            {
+                return -1;
+            }
+
+            if (fireStarter1.m_RequiresSunLight && !fireStarter2.m_RequiresSunLight)
+            {
+                return 1;
+            }
+
+            int chanceComparison = fireStarter1.m_FireStartSkillModifier.CompareTo(fireStarter2.m_FireStartSkillModifier);
+            if (chanceComparison != 0)
+            {
+                return -chanceComparison;
+            }
+
+            return item1.m_DisplayName.CompareTo(item2.m_DisplayName);
+        }
+    }
+}
