c2df55a7d213d74cf780e12532c9ad6111f03bde
WulfMarius <WulfMarius@users.noreply.github.com>
2018-12-19 14:42:21 +0100

    updated to work with TLD v1.41

---
+++ src/Implementation.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace SortFireStarters
+{
+    public class Implementation
+    {
+        public const string NAME = "Sort-Fire-Starters";
+
+        public static void OnLoad()
+        {
+            AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
+            Log("Version " + assemblyName.Version);
+        }
+
+        internal static void Log(string message)
+        {
+            Debug.LogFormat("[" + NAME + "] {0}", message);
+        }
+
+        internal static void Log(string message, params object[] parameters)
+        {
+            string preformattedMessage = string.Format("[" + NAME + "] {0}", message);
+            Debug.LogFormat(preformattedMessage, parameters);
+        }
+    }
+}
